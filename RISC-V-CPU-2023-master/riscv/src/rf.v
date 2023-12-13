6a2f21c8f894f72a7e93bbe40a12b703d6a4af5f
Irfnfnkemed <Irfnfnkemed@sjtu.edu.cn>
2023-12-13 15:43:54 +0800

    feat: finish reservation station (without debugging)

--- RISC-V-CPU-2023-master/riscv/src/rf.v
+++ RISC-V-CPU-2023-master/riscv/src/rf.v
@@ -9,44 +9,44 @@ module register_file #(
     input wire rdy_in,  // ready signal, pause cpu when low
 
     // from instr latch (issue)
-    input wire instr_signal,  //1 for fetching registers
-    input wire [REG_WIDTH-1:0] rs_id_1,
-    input wire [REG_WIDTH-1:0] rs_id_2,
-    output wire [REG_WIDTH-1:0] rs_data_1,
-    output wire [REG_WIDTH-1:0] rs_data_2,
-    output wire [ROB_WIDTH-1:0] rs_tag_1,
-    output wire [ROB_WIDTH-1:0] rs_tag_2,
-    output wire rs_valid_1,
-    output wire rs_valid_2,
-    input wire [REG_WIDTH-1:0] rd_id,
-    input wire [ROB_WIDTH-1:0] rd_tag,  // overwrite the tag of rd
+    input  wire                  instr_signal,  //1 for fetching registers
+    input  wire [`REG_WIDTH-1:0] rs_id_1,
+    input  wire [`REG_WIDTH-1:0] rs_id_2,
+    output wire [`REG_WIDTH-1:0] rs_value_1,
+    output wire [`REG_WIDTH-1:0] rs_value_2,
+    output wire [ ROB_WIDTH-1:0] rs_tag_1,
+    output wire [ ROB_WIDTH-1:0] rs_tag_2,
+    output wire                  rs_valid_1,
+    output wire                  rs_valid_2,
+    input  wire [`REG_WIDTH-1:0] rd_id,
+    input  wire [ ROB_WIDTH-1:0] rd_tag,        // overwrite the tag of rd
 
     // from rob (commit)
     input wire rob_commit_signal,  //1 for committing
-    input wire [REG_WIDTH-1:0] commit_rd_data,
+    input wire [`REG_WIDTH-1:0] commit_rd_value,
     input wire [ROB_WIDTH-1:0] commit_rd_tag
 );
 
-  reg [REG_WIDTH-1:0] regs[REG_SIZE-1:0];  // registers
-  reg [ROB_WIDTH-1:0] tags[REG_SIZE-1:0];  // register tags
-  reg valid[REG_SIZE-1:0];  // validation of get register data, 1 for valid (invalid tag), 0 for invalid(valid tag)
+  reg [`REG_WIDTH-1:0] values[`REG_SIZE-1:0];  // registers
+  reg [ROB_WIDTH-1:0] tags[`REG_SIZE-1:0];  // register tags
+  reg valid[`REG_SIZE-1:0];  // validation of get register value, 1 for valid (invalid tag), 0 for invalid(valid tag)
 
   assign sign_1 = rob_commit_signal & ~valid[rs_id_1] & (tags[rs_id_1] == commit_rd_tag);// 1 when rob commitment update rs1 at the same time
   assign sign_2 = rob_commit_signal & ~valid[rs_id_2] & (tags[rs_id_2] == commit_rd_tag);// 1 when rob commitment update rs2 at the same time
-  assign rs_data_1 = ({REG_WIDTH{sign_1}} & commit_rd_data) | ({REG_WIDTH{~sign_1}} & regs[rs_id_1]) ; // fowarding when rob commitment update rs1 at the same time
-  assign rs_data_2 = ({REG_WIDTH{sign_2}} & commit_rd_data) | ({REG_WIDTH{~sign_2}} & regs[rs_id_2]) ; // fowarding when rob commitment update rs2 at the same time
+  assign rs_value_1 = ({`REG_WIDTH{sign_1}} & commit_rd_value) | ({`REG_WIDTH{~sign_1}} & values[rs_id_1]) ; // fowarding when rob commitment update rs1 at the same time
+  assign rs_value_2 = ({`REG_WIDTH{sign_2}} & commit_rd_value) | ({`REG_WIDTH{~sign_2}} & values[rs_id_2]) ; // fowarding when rob commitment update rs2 at the same time
   assign rs_tag_1 = tags[rs_id_1];
   assign rs_tag_2 = tags[rs_id_2];
   assign rs_valid_1 = sign_1 | (~sign_1 & valid[rs_id_1]);
   assign rs_valid_2 = sign_2 | (~sign_2 & valid[rs_id_2]);
 
-  integer i;
+  integer i_reset;
   always @(posedge clk_in) begin  // reset register file
     if (rst_in) begin
-      for (i = 0; i < REG_SIZE; i = i + 1) begin
-        regs[i]  <= {REG_WIDTH{1'b0}};
-        tags[i]  <= {ROB_WIDTH{1'b1}};
-        valid[i] <= 1'b0;
+      for (i_reset = 0; i_reset < `REG_SIZE; i_reset = i_reset + 1) begin
+        values[i_reset] <= {`REG_WIDTH{1'b0}};
+        tags[i_reset]   <= {ROB_WIDTH{1'b0}};
+        valid[i_reset]  <= 1'b0;
       end
     end
   end
@@ -58,12 +58,13 @@ module register_file #(
     end
   end
 
-  always @(posedge clk_in) begin  // remove tag and updating data when matching the tag and instr latching doesn't put new tag on rd
+  integer i_commit;
+  always @(posedge clk_in) begin  // removing tag and updating value when matching the tag and instr latching doesn't put new tag on rd
     if (rdy_in & rob_commit_signal) begin
-      for (i = 0; i < REG_SIZE; i = i + 1) begin
-        if (~valid[i] & commit_rd_tag == tags[i] & ~(instr_signal & rd_id == i)) begin
-          valid[i] <= 1'b1;
-          regs[i]  <= commit_rd_data;
+      for (i_commit = 0; i_commit < `REG_SIZE; i_commit = i_commit + 1) begin
+        if (~valid[i_commit] & (commit_rd_tag == tags[i_commit]) & ~(instr_signal & rd_id == i_commit)) begin
+          valid[i_commit]  <= 1'b1;
+          values[i_commit] <= commit_rd_value;
         end
       end
     end
--- RISC-V-CPU-2023-master/riscv/src/rs.v
+++ RISC-V-CPU-2023-master/riscv/src/rs.v
@@ -1,8 +1,153 @@
-module reservation_station(
+`define REG_WIDTH 5
+`define OPCODE_WIDTH 5
+
+module reservation_station #(
+    parameter RS_WIDTH  = 4,
+    parameter ROB_WIDTH = 4,
+    parameter RS_SIZE   = 2 ** RS_WIDTH
+) (
     input wire clk_in,  // system clock signal
     input wire rst_in,  // reset signal
-    input wire rdy_in   // ready signal, pause cpu when low
+    input wire rdy_in,  // ready signal, pause cpu when low
 
+    //issued instr from instr latch (fetch value/tag from register file)
+    input wire issue,  // 1 for issuing instruction 
+    input wire [`OPCODE_WIDTH-1 : 0] opcode_issue,
+    input wire [`REG_WIDTH-1 : 0] rs_issue_value_1,
+    input wire [`REG_WIDTH-1 : 0] rs_issue_value_2,
+    input wire [ROB_WIDTH-1 : 0] rs_issue_tag_1,
+    input wire [ROB_WIDTH-1 : 0] rs_issue_tag_2,
+    input wire rs_issue_valid_1,
+    input wire rs_issue_valid_2,
+    input wire [ROB_WIDTH-1 : 0] rd_issue_tag,
+
+    //output data for ALU calculating, supporting two ALUs
+    output reg busy_alu_1,  // 1 for sending calulating task to ALU
+    output reg busy_alu_2,  // 1 for sending calulating task to ALU
+    output reg [`OPCODE_WIDTH-1:0] opcode_alu_1,
+    output reg [`OPCODE_WIDTH-1:0] opcode_alu_2,
+    output reg [`REG_WIDTH-1 : 0] lhs_alu_1,
+    output reg [`REG_WIDTH-1 : 0] lhs_alu_2,
+    output reg [`REG_WIDTH-1 : 0] rhs_alu_1,
+    output reg [`REG_WIDTH-1 : 0] rhs_alu_2,
+    output reg [ROB_WIDTH-1:0] rd_tag_alu_1,
+    output reg [ROB_WIDTH-1:0] rd_tag_alu_2,
+
+    // commitment from ROB, flushing rs
+    input wire commit,  // 1 for committing
+    input wire [`REG_WIDTH-1 : 0] commit_value,
+    input wire [ROB_WIDTH-1 : 0] commit_tag,
+
+    output wire full  // 1 for RS is full
 );
+
+  //RS lines
+  reg busy[RS_SIZE-1:0];  //1 for busy
+  reg [`OPCODE_WIDTH-1 : 0] opcode[RS_SIZE-1:0];  // opcode for ALU calculation categories
+  reg [`REG_WIDTH-1 : 0] rs_value_1[RS_SIZE-1:0];
+  reg [`REG_WIDTH-1 : 0] rs_value_2[RS_SIZE-1:0];
+  reg [ROB_WIDTH-1 : 0] rs_tag_1[RS_SIZE-1:0];
+  reg [ROB_WIDTH-1 : 0] rs_tag_2[RS_SIZE-1:0];
+  reg rs_valid_1[RS_SIZE-1:0];  // 1 for rs_1 value is valid
+  reg rs_valid_2[RS_SIZE-1:0];  // 1 for rs_2 value is valid
+  reg [ROB_WIDTH-1 : 0] rd_tag[RS_SIZE-1:0];
+  reg rd_valid[RS_SIZE-1:0];  // 1 for rd value is valid
+  wire [RS_WIDTH-1 : 0] free_pos;  // free position when RS is not full 
+
+  // assign to get the free position in RS
+  genvar i_select;
+  generate
+    wire [RS_WIDTH-1:0] select_pos[RS_SIZE-1:1];
+    wire valid_pos[RS_SIZE-1:1];  //1 for valid(free)
+    // if first line is free, select_pos is set to its index; else if second line is free, select_pos is set to its index
+    // if both lines are busy, set valid_pos to 0; else set it to 1
+    for (i_select = RS_SIZE / 2; i_select < RS_SIZE; i_select = i_select + 1) begin
+      assign tmp1 = (i_select - 8) << 1;
+      assign tmp2 = tmp1 + 1;
+      assign select_pos[i_select] = ({RS_WIDTH{~busy[tmp1]}} & tmp1) |
+                                ({RS_WIDTH{busy[tmp1]}} & {RS_WIDTH{~busy[tmp2]}} & tmp2);
+      assign valid_pos[i_select] = ~busy[tmp1] | ~busy[tmp2];
+    end
+    for (i_select = 1; i_select < RS_SIZE / 2; i_select = i_select + 1) begin
+      assign tmp1 = i_select << 1;
+      assign tmp2 = tmp1 + 1;
+      assign select_pos[i_select] = ({RS_WIDTH{~valid_pos[tmp1]}} & select_pos[tmp1]) |
+                                ({RS_WIDTH{valid_pos[tmp1]}} & {RS_WIDTH{~valid_pos[tmp2]}} & select_pos[tmp2]);
+      assign valid_pos[i_select] = ~(busy[tmp1] & busy[tmp2]);
+    end
+    assign full = valid_pos[1];
+    assign free_pos = select_pos[1];
+  endgenerate
+
+
+
+  integer i;
+
+  always @(posedge clk_in) begin  // reset register file
+    if (rst_in) begin
+      for (i = 0; i < RS_SIZE; i = i + 1) begin
+        busy[i]       <= 1'b0;
+        rs_valid_1[i] <= 1'b0;
+        rs_valid_2[i] <= 1'b0;
+        busy_alu_1    <= 1'b0;
+        busy_alu_2    <= 1'b0;
+      end
+    end
+  end
+
+  always @(posedge clk_in) begin  // issue an instruction
+    if (rdy_in & issue) begin
+      busy[free_pos]       <= 1'b1;
+      opcode[free_pos]     <= opcode_issue;
+      rs_value_1[free_pos] <= rs_issue_value_1;
+      rs_valid_2[free_pos] <= rs_issue_value_2;
+      rs_tag_1[free_pos]   <= rs_issue_tag_1;
+      rs_tag_2[free_pos]   <= rs_issue_tag_2;
+      rs_valid_1[free_pos] <= rs_issue_valid_1;
+      rs_valid_2[free_pos] <= rs_issue_valid_2;
+      rd_tag[free_pos]     <= rd_issue_tag;
+    end
+  end
+
+  integer i_commit;
+  always @(posedge clk_in) begin  // flush rs values according to the commitment
+    for (i_commit = 0; i_commit < RS_SIZE; i_commit = i_commit + 1) begin
+      if (busy[i]) begin
+        if (~rs_valid_1[i] && (rs_tag_1[i] == commit_tag)) begin
+          rs_valid_1[i] <= 1'b1;
+          rs_value_1[i] <= commit_value;
+        end
+        if (~rs_valid_2[i] && (rs_tag_2[i] == commit_tag)) begin
+          rs_valid_2[i] <= 1'b1;
+          rs_value_2[i] <= commit_value;
+        end
+      end
+    end
+  end
+
+  integer i_alu;
+  always @(posedge clk_in) begin  // send valid instr to ALU when ALU is free, and free the RS line at the same time
+    if (rdy_in) begin
+      for (i_alu = 0; i_alu < RS_SIZE; i_alu = i_alu + 1) begin
+        if (rs_valid_1[i] & rs_valid_2[i]) begin
+          if (~busy_alu_1) begin  // calculate in ALU1
+            busy_alu_1    = 1'b1; // block assignment, avoiding more than one lines sending data to ALU at the same time
+            busy[i]      <= 1'b0;
+            opcode_alu_1 <= opcode[i];
+            lhs_alu_1    <= rs_value_1[i];
+            rhs_alu_1    <= rs_valid_2[i];
+            rd_tag_alu_1 <= rd_tag[i];
+          end else if (~busy_alu_2) begin  // calculate in ALU2
+            busy_alu_2    = 1'b1; // block assignment, avoiding more than one lines sending data to ALU at the same time
+            busy[i]      <= 1'b0;
+            opcode_alu_2 <= opcode[i];
+            lhs_alu_2    <= rs_value_1[i];
+            rhs_alu_2    <= rs_valid_2[i];
+            rd_tag_alu_2 <= rd_tag[i];
+          end
+        end
+      end
+    end
+  end
 
 endmodule
