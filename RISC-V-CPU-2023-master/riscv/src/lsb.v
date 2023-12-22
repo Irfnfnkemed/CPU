a08e4dd11bf28328459b87e4d697711f2e4e3de7
Irfnfnkemed <Irfnfnkemed@sjtu.edu.cn>
2023-12-22 14:21:48 +0800

    fix: signed/unsigned load

--- RISC-V-CPU-2023-master/riscv/src/lsb.v
+++ RISC-V-CPU-2023-master/riscv/src/lsb.v
@@ -13,9 +13,9 @@ module load_store_buffer #(
     // for data, valid bit isn't necessary: 1 for load, 0 for store(shift to 1 when committing)
     input wire issue_signal,  // 1 for issuing an instruction
     input wire issue_wr,  // 1 for store, 0 for load
-    input wire [1:0] issue_len,
+    input wire issue_signed,  //1 for signed load, 0 for unsigned load
+    input wire [1:0] issue_len,  // length of load/store instr (00:1 byte, 01:2 bytes, 11:4 bytes)
     input wire [31:0] issue_addr,
-    input wire [31:0] issue_value,
     input wire [ROB_WIDTH-1:0] issue_tag_addr,
     input wire [ROB_WIDTH-1:0] issue_tag_rd,
     input wire issue_valid_addr,  // 1 for addr valid (tag is invalid)
@@ -51,10 +51,10 @@ module load_store_buffer #(
   // tag_value is unnecessary, because it's useless for loading and value will be updated without tag when committing for store 
   reg busy[LSB_SIZE-1:0];  // 1 for line busy
   reg ready[LSB_SIZE-1:0];  // 1 for ready
-  reg wr[LSB_SIZE-1:0];
+  reg wr[LSB_SIZE-1:0];  // 1 for store
   reg [1:0] len[LSB_SIZE-1:0];
   reg [31:0] address[LSB_SIZE-1:0];
-  reg [31:0] value[LSB_SIZE-1:0];
+  reg [31:0] value[LSB_SIZE-1:0];  // for load task, value[0]: 1->signed load; 0->unsigned load
   reg [ROB_WIDTH-1:0] tag_addr[LSB_SIZE-1:0];
   reg [ROB_WIDTH-1:0] tag_rd[LSB_SIZE-1:0];
 
@@ -92,7 +92,7 @@ module load_store_buffer #(
         status <= 1'b0;
       end
       for (i_clear = 0; i_clear < LSB_SIZE; i_clear = i_clear + 1) begin
-        if(~(busy[i_clear] & wr[i_clear] & ready[i_clear]))begin // clear LSB, expect reafy store task
+        if(~(busy[i_clear] & wr[i_clear] & ready[i_clear]))begin // clear LSB, except ready store task
           busy[i_clear]  <= 1'b0;
           ready[i_clear] <= 1'b0;
         end
@@ -105,27 +105,12 @@ module load_store_buffer #(
       busy[rear] <= 1'b1;
       ready[rear] <= issue_valid_addr & ~issue_wr; // for loading, ready bit is same to valid_addr, for storing, ready bit is 0
       wr[rear] <= issue_wr;
+      value[rear][0] <= issue_signed;
       len[rear] <= issue_len;
       address[rear] <= issue_addr;
-      value[rear] <= issue_value;
       tag_addr[rear] <= issue_tag_addr;
       tag_rd[rear] <= issue_tag_rd;
       rear <= rear + 1;
-    end
-  end
-
-  integer i_commit;
-  always @(posedge clk_in) begin
-    if (rdy_in & commit_signal) begin
-      for (i_commit = 0; i_commit < LSB_SIZE; i_commit = i_commit + 1) begin
-        if (busy[i_commit]) begin  // only update the value and set store line to ready, because data should be updated before
-          if (~ready[i_alu] & (tag_rd[i_commit] == commit_tag)) begin
-            ready[i_alu] <= 1'b1;
-            value[i_alu] <= commit_value;
-            last_store_commit <= i_alu;
-          end
-        end
-      end
     end
   end
 
@@ -149,7 +134,7 @@ module load_store_buffer #(
         front <= front + 1;  // free line
         busy[front] <= 1'b0;
         ready[front] <= 1'b0;
-        if (wr[front]) begin  // send data&tag to RS&ROB, flush data&tag in LSB
+        if (~wr[front]) begin  // for load task, send data&tag to RS&ROB, flush data&tag in LSB
           for (i_mem = 0; i_mem < LSB_SIZE; i_mem = i_mem + 1) begin
             if (busy[i_mem]) begin  // update LSB (addr)
               if (~ready[i_mem] & (tag_addr[i_mem] == tag_rd[front])) begin
@@ -159,11 +144,38 @@ module load_store_buffer #(
             end
           end
           done_signal <= 1'b1;
-          done_value <= mem_din;
           done_tag <= tag_rd[front];
+          case (len[front])
+            2'b00: begin
+              done_value[31:8] <= value[front][0] ? {24{mem_din[7]}} : {24{1'b0}};
+              done_value[7:0]  <= mem_din[7:0];
+            end
+            2'b01: begin
+              done_value[31:16] <= value[front][0] ? {16{mem_din[15]}} : {24{1'b0}};
+              done_value[15:0]  <= mem_din[15:0];
+            end
+            2'b00: begin
+              done_value <= mem_din;
+            end
+          endcase
         end
       end else begin  // handle done signal, avoiding flush RS/ROB more than one time
         done_signal <= 1'b0;
+      end
+    end
+  end
+
+  integer i_commit;
+  always @(posedge clk_in) begin
+    if (rdy_in & commit_signal) begin
+      for (i_commit = 0; i_commit < LSB_SIZE; i_commit = i_commit + 1) begin
+        if (busy[i_commit]) begin  // only update the value and set store line to ready, because data should be updated before
+          if (~ready[i_alu] & (tag_rd[i_commit] == commit_tag)) begin
+            ready[i_commit]   <= 1'b1;
+            value[i_commit]   <= commit_value;
+            last_store_commit <= i_commit;
+          end
+        end
       end
     end
   end
